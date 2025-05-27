4ac7f40ae5f6d68af87f912075a1932a4216a58c
FazeNCode <FazeNCode@gmail.com>
2025-05-27 03:02:44 -0400

    Update vehicles page layout and integrate vehicle data display

--- src/app/vehicles/page.jsx
+++ src/app/vehicles/page.jsx
@@ -1,42 +1,4 @@
-// "use client";
 
-// import { useEffect, useState } from 'react';
-
-// export default function VehiclesPage() {
-//   const [vehicles, setVehicles] = useState([]);
-
-//   useEffect(() => {
-//     const fetchVehicles = async () => {
-//       try {
-//         const response = await fetch('/api/vehicles');
-//         const data = await response.json();
-//         if (data.success) {
-//           setVehicles(data.data);
-//         }
-//       } catch (error) {
-//         console.error('Error fetching vehicles:', error);
-//       }
-//     };
-
-//     fetchVehicles();
-//   }, []);
-
-//   return (
-//     <div>
-//       <h1>Vehicle</h1>
-//       <ul>
-//         {vehicles.map((vehicle) => (
-//           <li key={vehicle.vin}>
-//             {vehicle.year} {vehicle.make} {vehicle.model} - {vehicle.color} ({vehicle.mileage} miles)
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
-
-
-// src/app/vehicles/page.jsx
 'use client';
 
 import { useEffect, useState } from 'react';
